f80ed2fb58f24066974a773e4b355c3e438c456e
makedonzp <freefilosof@gmail.com>
2024-10-29 19:21:23 +0300

    dataConcext correct

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import styles from "./Layout.module.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
@@ -18,22 +18,52 @@ import About from "../About/About";
 import LandDetails from "../District/LandDetails/LandDetails";
 import HotForm from "../HotForm/HotForm";
 import ValueFormRequest from "../ValueFormRequest/ValueFormRequest";
+import { DataContext } from "../DataContext/DataContext";
 import ScrollToTop from "../ScrollToTop/ScrollToTop";
 
-export default function Layout() {
+export default function Layout({ isMetrikaReady }) {
+  const data = useContext(DataContext);
   const location = useLocation();
+  const navigate = useNavigate();
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
   useEffect(() => {
-    // Отправка события просмотра страницы в Яндекс.Метрику
-    window.ym(98761584, "hit", location.pathname, {
-      title: document.title,
-      referer: document.referrer,
-      params: {
-        param1: "value1",
-        param2: "value2",
-      },
-    });
-  }, [location]);
+    const formSubmitted = localStorage.getItem("formSubmitted");
+    console.log("Проверка статуса отправки формы в Layout:", formSubmitted);
+
+    if (formSubmitted === "true") {
+      setIsFormSubmitted(true);
+    } else if (location.pathname === "/we-will-connect") {
+      console.log("Форма не отправлена, перенаправление на главную");
+      navigate("/");
+    }
+
+    if (location.pathname === "/") {
+      localStorage.removeItem("formSubmitted");
+    }
+  }, [location, navigate]);
+
+  useEffect(() => {
+    if (isMetrikaReady) {
+      console.log(
+        "Отправка события просмотра страницы для:",
+        location.pathname
+      );
+      window.ym(98761584, "hit", location.pathname, {
+        title: document.title,
+        referer: document.referrer,
+        params: {
+          param1: "value1",
+          param2: "value2",
+        },
+      });
+    } else {
+      console.log(
+        "Яндекс.Метрика не готова, не отправляем событие просмотра страницы для:",
+        location.pathname
+      );
+    }
+  }, [location, isMetrikaReady]);
 
   return (
     <div
@@ -46,26 +76,38 @@ export default function Layout() {
       <Header />
       <Routes>
         <Route path="/" exact element={<Main />} />
-        <Route path="/new" exact element={<New />} />
-        <Route path="/plots" element={<Lands />} />
-        <Route path="/plots/:citySlug" element={<District />} />
-        <Route path="/plots/:citySlug/:districtSlug" element={<Land />} />
+        <Route path="/new" exact element={<New data={data.new} />} />
+        <Route path="/plots" element={<Lands data={data.plots} />} />
+        <Route
+          path="/plots/:citySlug"
+          element={<District data={data.plots} />}
+        />
+        <Route
+          path="/plots/:citySlug/:districtSlug"
+          element={<Land data={data.plots} />}
+        />
         <Route
           path="/plots/:citySlug/:districtSlug/:landSlug"
-          element={<LandDetails />}
+          element={<LandDetails data={data.plots} />}
         />
         <Route path="/services" element={<Services />} />
         <Route path="/confeditial" element={<Confeditial />} />
-        <Route path="/new/:citySlug" element={<City />} />
-        <Route path="/new/:citySlug/:complexSlug" element={<Complex />} />
+        <Route path="/new/:citySlug" element={<City data={data.new} />} />
+        <Route
+          path="/new/:citySlug/:complexSlug"
+          element={<Complex data={data.new} type="complexes" />}
+        />
         <Route
           path="/new/:citySlug/:complexSlug/:apartmentSlug"
-          element={<ApartmentDetails />}
+          element={<ApartmentDetails data={data.new} />}
         />
         <Route path="/contacts" element={<Contacts />} />
         <Route path="/about" element={<About />} />
         <Route path="/hot-form" element={<HotForm />} />
-        <Route path="/we-will-connect" element={<ValueFormRequest />} />
+        <Route
+          path="/we-will-connect"
+          element={isFormSubmitted ? <ValueFormRequest /> : <Main />}
+        />
       </Routes>
       <Footer />
     </div>
